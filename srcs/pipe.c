085520cdf8610732e61461b784cee5e718bc4a1d
MatheusCervieri <mathcervieri@gmail.com>
2022-08-01 22:37:41 +0200

    Norm fixes

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 21:48:39 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/01 21:52:01 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/01 22:35:04 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,24 @@ int	open_file(char *file_name, t_data *data)
 		exit_program("Error when open the file!", data);
 	return (fd);
 }
+void pid_one_func(t_data *data, int in_file_fd, int fd[2])
+{
+	dup2(in_file_fd, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	open_program(data->program1_path, data->input_program_parameters, data);
+}
+
+void pid_two_func(t_data *data, int out_file_fd, int fd[2])
+{
+	dup2(out_file_fd, STDOUT_FILENO);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	open_program(data->program2_path,
+			data->output_program_parameters, data);
+}
 
 void	pipe_operator(t_data *data)
 {
@@ -45,25 +63,13 @@ void	pipe_operator(t_data *data)
 	pid1 = fork();
 	if (pid1 < 0)
 		exit_program("Fork function error", data);
-
 	if (pid1 == 0)
-	{
-		dup2(in_file_fd, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		open_program(data->program1_path, data->input_program_parameters, data);
-	}
+		pid_one_func(data, in_file_fd, fd);		
 	pid2 = fork();
 	if (pid2 == 0)
-	{
-		dup2(out_file_fd, STDOUT_FILENO);
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		open_program(data->program2_path, data->output_program_parameters, data);
-	}
+		pid_two_func(data, out_file_fd, fd);
 	close(in_file_fd);
+	// close out_file_fd
 	close(fd[0]);
 	close(fd[1]);
 	waitpid(pid1, NULL, 0);
